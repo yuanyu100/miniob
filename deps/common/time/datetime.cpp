88a152d64ac295f7b64d20b873f18d2f8a0d72c8
yuanyu100 <1990899448@qq.com>
2024-11-11 04:08:46 +0800

    [yuanyu100test02]

--- deps/common/time/datetime.cpp
+++ deps/common/time/datetime.cpp
@@ -440,5 +440,12 @@ bool DateTime::is_valid_xml_datetime(const string &str)
 
   return true;
 }
+bool check_date(int y,int m,int d){
+  static int mon[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+  bool leap=(y%400 || (y%100 && y%4==0));
+  return y>0 && y<=9999
+        && (m>0) &&(m<=12)
+        && (d>0) &&(d<=((m==2&& leap)?1:0)+mon[m]);
+}
 
 }  // namespace common
